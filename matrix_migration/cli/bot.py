646a81e396e8e285412a0b5258d5683b5295f136
bdvil <benjam.devillers@gmail.com>
2024-02-03 16:17:14 +0100

    Add as response to ping

--- matrix_migration/cli/bot.py
+++ matrix_migration/cli/bot.py
@@ -1,9 +1,10 @@
 import asyncio
+from aiohttp.web import Application, Request, Response, run_app
 import click
 
 from uuid import uuid4
 
-from matrix_migration.config import load_config
+from matrix_migration.config import Config, load_config
 from aiohttp import ClientSession, web
 
 
@@ -16,9 +17,21 @@ async def ping(hs_url: str, as_id: str, as_token: str):
             print(response)
 
 
-async def handle(request: web.Request) -> web.Response:
+def check_headers(request: Request, hs_token: str) -> bool:
+    return ("Authorization" in request.headers.keys()
+            and request.headers["Authorization"] == f"Bearer {hs_token}")
+
+
+async def handle_ping(request: Request) -> Response:
+    config: Config = request.app["config"]
+    if check_headers(request, config.hs_token):
+        return Response(status=200)
+    return Response(status=403)
+
+
+async def handle(request: Request) -> Response:
     print(request.query_string)
-    return web.Response()
+    return Response()
 
 
 @click.command("serve")
@@ -26,7 +39,8 @@ def serve():
     config = load_config()
     print(config.model_dump())
     asyncio.run(ping(config.homeserver_from.url, config.as_id, config.as_token))
-    app = web.Application()
+    app = Application()
+    app["config"] = config
     app.add_routes([
         web.put("/_matrix/app/v1/transactions/{txnId}", handle),
         web.post("/_matrix/app/v1/ping", handle),
@@ -38,4 +52,4 @@ def serve():
         web.get("/_matrix/app/v1/thirdparty/user", handle),
         web.get("/_matrix/app/v1/thirdparty/user/{protocol}", handle),
     ])
-    web.run_app(app, port=config.port)
+    run_app(app, port=config.port)
