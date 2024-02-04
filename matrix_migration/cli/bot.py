622be3ae03e03d4de3c637521c4f556357e00663
bdvil <benjam.devillers@gmail.com>
2024-02-04 11:24:12 +0100

    Add logging

--- matrix_migration/cli/bot.py
+++ matrix_migration/cli/bot.py
@@ -1,14 +1,17 @@
-from aiohttp.web import Application, Request, Response, run_app
 import click
-
-from uuid import uuid4
-
-import json
-from matrix_migration.config import Config, load_config
 from aiohttp import ClientResponse, ClientSession, web
+from aiohttp.web import Application, Request, Response, run_app
+
+from matrix_migration import LOGGER
+from matrix_migration.config import Config, load_config
 
 
-async def ping(hs_url: str, as_id: str, as_token: str, transaction_id: str | None = None) -> ClientResponse | None:
+async def ping(
+    hs_url: str,
+    as_id: str,
+    as_token: str,
+    transaction_id: str | None = None,
+) -> ClientResponse | None:
     ping_url = hs_url + f"/_matrix/client/v1/appservice/{as_id}/ping"
     headers = {
         "Authorization": f"Bearer {as_token}",
@@ -17,24 +20,31 @@ async def ping(hs_url: str, as_id: str, as_token: str, transaction_id: str | Non
     if transaction_id is not None:
         data["transaction_id"] = transaction_id
     async with ClientSession() as session:
-        print(f"POSTING here: {ping_url}")
-        async with session.post(ping_url, headers=headers, json=data) as response:
-            print("PING RESP")
-            print(response)
+        LOGGER.info("CLIENT ping")
+        async with session.post(
+            ping_url, headers=headers, json=data
+        ) as response:
             data = await response.json()
-            print("DATA")
-            print(data)
+            LOGGER.debug(
+                "CLIENT ping",
+                extra={"headers": response.headers, "body": data},
+            )
             return response
 
 
 def check_headers(request: Request, hs_token: str) -> bool:
-    print(request.headers)
-    return ("Authorization" in request.headers.keys()
-            and request.headers["Authorization"] == f"Bearer {hs_token}")
+    return (
+        "Authorization" in request.headers.keys()
+        and request.headers["Authorization"] == f"Bearer {hs_token}"
+    )
 
 
 async def handle_ping(request: Request) -> Response:
-    print("HANDLE PING")
+    body = await request.json()
+    LOGGER.info(
+        "SERVER ping",
+        extra={"url": request.url, "headers": request.headers, "body": body},
+    )
     config: Config = request.app["config"]
     if check_headers(request, config.hs_token):
         return web.json_response({}, status=200)
@@ -42,49 +52,62 @@ async def handle_ping(request: Request) -> Response:
 
 
 async def handle(request: Request) -> Response:
-    print("HANDLE")
-    print(request.url)
-    return Response()
+    body = await request.json()
+    LOGGER.info(
+        "SERVER ping",
+        extra={"url": request.url, "headers": request.headers, "body": body},
+    )
+    return web.json_response({}, status=200)
 
 
 async def handle_log(request: Request) -> Response:
-    print("HANDLE LOG")
-    print(request.url)
-    print(request.headers)
-    print("DATA")
-    data = await request.json()
-    print(data)
-    return Response()
+    body = await request.json()
+    LOGGER.info(
+        "SERVER ping",
+        extra={"url": request.url, "headers": request.headers, "body": body},
+    )
+    return web.json_response({}, status=200)
 
 
 async def handle_test(request: Request) -> Response:
     try:
         config: Config = request.app["config"]
-        resp = await ping(config.homeserver_from.url, config.as_id, config.as_token)
-        print("TEST - RESP")
-        print(resp)
-    except: 
-        return Response(status=500)
-    return Response(status=200)
+        resp = await ping(
+            config.homeserver_from.url, config.as_id, config.as_token
+        )
+        LOGGER.info(
+            "TEST success",
+            extra={
+                "url": request.url,
+                "headers": request.headers,
+                "resp": resp,
+            },
+        )
+    except Exception:
+        LOGGER.info("TEST failure")
+        return web.json_response({}, status=500)
+    return web.json_response({}, status=200)
 
 
 @click.command("serve")
 def serve():
     config = load_config()
-    print(config.model_dump())
+    LOGGER.debug("CONFIG", config.model_dump())
     app = Application()
     app["config"] = config
-    app.add_routes([
-        web.get("/test", handle_test),
-        web.post("/log", handle_log),
-        web.put("/_matrix/app/v1/transactions/{txnId}", handle),
-        web.post("/_matrix/app/v1/ping", handle_ping),
-        web.get("/_matrix/app/v1/users/{userId}", handle),
-        web.get("/_matrix/app/v1/rooms/{roomAlias}", handle),
-        web.get("/_matrix/app/v1/thirdparty/location", handle),
-        web.get("/_matrix/app/v1/thirdparty/location/{protocol}", handle),
-        web.get("/_matrix/app/v1/thirdparty/protocol/{protocol}", handle),
-        web.get("/_matrix/app/v1/thirdparty/user", handle),
-        web.get("/_matrix/app/v1/thirdparty/user/{protocol}", handle),
-    ])
+    app.add_routes(
+        [
+            web.get("/test", handle_test),
+            web.post("/log", handle_log),
+            web.put("/_matrix/app/v1/transactions/{txnId}", handle),
+            web.post("/_matrix/app/v1/ping", handle_ping),
+            web.get("/_matrix/app/v1/users/{userId}", handle),
+            web.get("/_matrix/app/v1/rooms/{roomAlias}", handle),
+            web.get("/_matrix/app/v1/thirdparty/location", handle),
+            web.get("/_matrix/app/v1/thirdparty/location/{protocol}", handle),
+            web.get("/_matrix/app/v1/thirdparty/protocol/{protocol}", handle),
+            web.get("/_matrix/app/v1/thirdparty/user", handle),
+            web.get("/_matrix/app/v1/thirdparty/user/{protocol}", handle),
+        ]
+    )
     run_app(app, port=config.port)
