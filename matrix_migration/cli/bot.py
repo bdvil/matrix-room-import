66778c0dcfcb770bc954c22d05470c6279ebf491
bdvil <benjam.devillers@gmail.com>
2024-02-14 19:57:17 +0100

    Fix wrong access_token attr

--- matrix_migration/cli/bot.py
+++ matrix_migration/cli/bot.py
@@ -93,6 +93,6 @@ def serve():
             client.update_bot_profile(config.bot_username, config.bot_displayname)
         )
         runner.run(client.login(config.bot_username))
-        if client.access_token is None:
+        if client.bot_infos.access_token is None:
             LOGGER.error("Could not login.")
         runner.run(main(app, config.port))
